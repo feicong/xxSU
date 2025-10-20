toraidl/KernelSU

manager:  failure mode dummy demo

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
@@ -283,31 +283,16 @@ private fun StatusCard(
                     }
                 }
 
-                kernelVersion.isGKI() -> {
-                    Icon(Icons.Outlined.Warning, stringResource(R.string.home_not_installed))
-                    Column(Modifier.padding(start = 20.dp)) {
-                        Text(
-                            text = stringResource(R.string.home_not_installed),
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                        Spacer(Modifier.height(4.dp))
-                        Text(
-                            text = stringResource(R.string.home_click_to_install),
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                    }
-                }
-
                 else -> {
-                    Icon(Icons.Outlined.Block, stringResource(R.string.home_unsupported))
+                    Icon(Icons.Outlined.Block, stringResource(R.string.home_failure))
                     Column(Modifier.padding(start = 20.dp)) {
                         Text(
-                            text = stringResource(R.string.home_unsupported),
+                            text = stringResource(R.string.home_failure),
                             style = MaterialTheme.typography.titleMedium
                         )
                         Spacer(Modifier.height(4.dp))
                         Text(
-                            text = stringResource(R.string.home_unsupported_reason),
+                            text = stringResource(R.string.home_failure_reason),
                             style = MaterialTheme.typography.bodyMedium
                         )
                     }

feicong/xxSU

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
@@ -508,31 +508,6 @@ private fun StatusCard(
                     }
                 }
             }
-
-            kernelVersion.isGKI() -> {
-                Card(
-                    onClick = {
-                        if (kernelVersion.isGKI()) onClickInstall()
-                    },
-                    showIndication = true,
-                    pressFeedbackType = PressFeedbackType.Sink
-                ) {
-                    BasicComponent(
-                        title = stringResource(R.string.home_not_installed),
-                        summary = stringResource(R.string.home_click_to_install),
-                        leftAction = {
-                            Icon(
-                                Icons.Rounded.ErrorOutline,
-                                stringResource(R.string.home_not_installed),
-                                modifier = Modifier
-                                    .padding(end = 16.dp),
-                                tint = colorScheme.onBackground,
-                            )
-                        }
-                    )
-                }
-            }
-
             else -> {
                 Card(
                     onClick = {
@@ -542,12 +517,12 @@ private fun StatusCard(
                     pressFeedbackType = PressFeedbackType.Sink
                 ) {
                     BasicComponent(
-                        title = stringResource(R.string.home_unsupported),
-                        summary = stringResource(R.string.home_unsupported_reason),
+                        title = stringResource(R.string.home_failure),
+                        summary = stringResource(R.string.home_failure_reason),
                         leftAction = {
                             Icon(
                                 Icons.Rounded.ErrorOutline,
-                                stringResource(R.string.home_unsupported),
+                                stringResource(R.string.home_failure),
                                 modifier = Modifier
                                     .padding(end = 16.dp),
                                 tint = colorScheme.onBackground,